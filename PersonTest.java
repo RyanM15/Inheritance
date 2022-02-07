9e6a6cd962144328b709780d09d095f6ccfabfac
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-02-07 09:37:15 -0500

    PersonTest class

---
+++ PersonTest.java
@@ -0,0 +1,24 @@
+public class PersonTest{
+	public static void main(String[] args){
+		
+		Person p1 = new Person("Daniel", 2006);
+		Person p2 = new Person("Ryan", 2008);	
+		
+		Scholar s1 = new Scholar("Science");
+		Scholar s2 = new Scholar("Spanish");
+		
+		Instructor i1 = new Instructor(100,000);
+		Instructor i2 = new Instructor(65,000);
+		
+		
+		
+	}
+		
+	
+
+
+
+
+
+
+}
