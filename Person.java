17b17e26f9fcef0bd5bae44b7c9c847e1f7b947e
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-02-07 09:35:44 -0500

    Person class

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person{
+	public String name;
+	public int yearBirth;
+	public Person(String name, int yearBirth){
+		
+		
+		
+	}
+	public String toString(){
+		return ("Name: " + name); //+ ", year of birth is: " + super.yearBirth + ", major is: " + major);
+		
+	}
+
+
+
+
+
+}
