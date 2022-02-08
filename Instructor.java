5ebd8ce371b2c63257face71c1c760db7b4b1faa
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-02-08 12:08:32 -0500

    Update Instructor.java

--- Instructor.java
+++ Instructor.java
@@ -1,11 +1,11 @@
 public class Instructor extends Person{
 	private double salary;
-	public Instructor(String name, int yearBirth, double salary){
+	public Instructor(String name, int yearBirth, double salary){ //constructor that sets instance variables 
 		super(name, yearBirth);
 		this.salary = salary;
 	}
-	public String toString(){
-		return ("The Instrictors name is: " + super.name + ", year of birth is: " + super.yearBirth + ", salary is: " + salary);
+	public String toString(){ //converts variables to string so they can print 
+		return ("The Instructors name is: " + super.name + ", year of birth is: " + super.yearBirth + ", salary is: " + salary);
 		
 	}
 	
