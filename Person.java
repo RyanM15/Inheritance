c7d2089f3a634d88b9bdb143bf8af9da9724caf4
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-02-08 12:08:51 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,13 +1,15 @@
 public class Person{
 	public String name;
 	public int yearBirth;
-	public Person(String name, int yearBirth){
-		
+	
+	public Person(String name, int yearBirth){ //constructor that sets instance variables 
+		this.name = name;
+		this.yearBirth = yearBirth;
 		
 		
 	}
-	public String toString(){
-		return ("Name: " + name); //+ ", year of birth is: " + super.yearBirth + ", major is: " + major);
+	public String toString(){ //converts variables to string so they can print 
+		return ("Name: " + name + ", Year of birth is: " + yearBirth); //+ ", year of birth is: " + super.yearBirth + ", major is: " + major);
 		
 	}
 
@@ -16,3 +18,6 @@ public class Person{
 
 
 }
+
+
+
