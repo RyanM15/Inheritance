b0eaa52c6e85f0e18e102ed6ac03e667d88a07fd
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-02-08 12:09:15 -0500

    Update PersonTest.java

--- PersonTest.java
+++ PersonTest.java
@@ -1,15 +1,24 @@
 public class PersonTest{
-	public static void main(String[] args){
+	public static void main(String[] args){ //tester file that sets the variables 
 		
+		//sets variables for person class
 		Person p1 = new Person("Daniel", 2006);
 		Person p2 = new Person("Ryan", 2008);	
 		
-		Scholar s1 = new Scholar("Science");
-		Scholar s2 = new Scholar("Spanish");
+		//sets variables for scholar class
+		Scholar s1 = new Scholar("Kai", 2019, "Science");
+		Scholar s2 = new Scholar("Gabe", 2015, "Spanish");
 		
-		Instructor i1 = new Instructor(100,000);
-		Instructor i2 = new Instructor(65,000);
+		//sets variables for instructor class
+		Instructor i1 = new Instructor("Danny", 2011, 100000);
+		Instructor i2 = new Instructor("Logan", 2007, 65000);
 		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(i1);
+		System.out.println(i2);
 		
 		
 	}
