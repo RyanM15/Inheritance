40789b938cbade88b36b9edafaf7e7aa3811085d
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-02-08 12:09:34 -0500

    Update Scholar.java

--- Scholar.java
+++ Scholar.java
@@ -1,10 +1,10 @@
 public class Scholar extends Person{
 	private String major;
-	public Scholar(String name, int yearBirth, String major){
+	public Scholar(String name, int yearBirth, String major){ //constructor to set instance variables
 		super(name, yearBirth);
 		this.major = major;
 	}
-	public String toString(){
+	public String toString(){ //converts variables to string so they can print 
 		return ("The scholars name is: " + super.name + ", year of birth is: " + super.yearBirth + ", major is: " + major);
 		
 	}
